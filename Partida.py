5170997d83e36698b4ec99eff7f6a419634dd05c
Lluís Vivó <lluis.vivo.pons@gmail.com>
2026-01-03 13:17:38 +0100

    Versio 2.3
    S'ha eliminat el mode supervivencia per 2 jugadors

--- Partida.py
+++ Partida.py
@@ -6,17 +6,19 @@ from animacio import *
 
 def partida():
     juguem_de_nou = True
+    num_jugadors = 0
     while juguem_de_nou:
-        num_jugadors = crearSeleccions()
-        mode_escollit = crearMenu()
+        if num_jugadors == 0:
+            num_jugadors = crearSeleccions()
+        mode_escollit = crearMenu(num_jugadors)
         if mode_escollit[0] == 0:
             break
         elif mode_escollit[0] == 4:
             continue
         else:
-            if mode_escollit[1] == 0:
+            if mode_escollit[0]!= 1 and mode_escollit[1] == 0:
                 break
-            
+
             if num_jugadors == 1:
                 dades_finals = animacio(mode_escollit[0],mode_escollit[1])
             else:
--- interficie2.py
+++ interficie2.py
@@ -10,16 +10,16 @@ Noms_modes = {
 
 Descripcions = {
     1: "Condueix sense límits ni temps.\nIdeal per practicar.",
-    2: "Completa les voltes que triis el més ràpid possible.\nBat el teu rècord!",
+    2: "Completa les voltes que triis el més ràpid possible.",
     3: "Tens les vides que vulguis.\nSi xoques contra un cotxe o si estàs fora de la carretera per 5 segons,\n perds una vida."
 }
 
-def confirmarVoltes(coses_seleccionades,finestra,mode):
+def confirmarCoses(coses_seleccionades,finestra,mode):
     mode[1] = coses_seleccionades
     finestra.destroy()
 
 
-def destrueixFinestra(mode_escollit, menu, mode):
+def destrueixFinestra(mode_escollit, menu, mode,num_jugadors = 1):
     mode[0] = mode_escollit
     menu.destroy()
     if mode_escollit == 2:
@@ -37,7 +37,7 @@ def destrueixFinestra(mode_escollit, menu, mode):
 
         Button(finestra_voltes, text="COMENÇAR!", font=("Helvetica", 12, "bold"),
            bg="#00FF00", fg="black", cursor="hand2",
-           command=lambda: confirmarVoltes(selector.get(), finestra_voltes, mode)).pack(pady=10)
+           command=lambda: confirmarCoses(selector.get(), finestra_voltes, mode)).pack(pady=10)
 
         peu_de_pagina_guia = Label(finestra_voltes, text="Tanqui la finestra per tornar a la selecció de mode", font=("Arial", 10), 
                    bg="#1a1a1a", fg="gray", pady=20)
@@ -61,7 +61,7 @@ def destrueixFinestra(mode_escollit, menu, mode):
 
         Button(finestra_vides, text="COMENÇAR!", font=("Helvetica", 12, "bold"),
            bg="#00FF00", fg="black", cursor="hand2",
-           command=lambda: confirmarVoltes(selector.get(), finestra_vides, mode)).pack(pady=10)
+           command=lambda: confirmarCoses(selector.get(), finestra_vides, mode)).pack(pady=10)
 
         peu_de_pagina_guia = Label(finestra_vides, text="Tanqui la finestra per tornar a la selecció de mode", font=("Arial", 10), 
                    bg="#1a1a1a", fg="gray", pady=20)
@@ -77,11 +77,14 @@ def destrueixFinestra(mode_escollit, menu, mode):
         titol = Label(guia, text="Instruccions", font=("Helvetica", 24, "bold"), 
                    bg="#1a1a1a", fg="#00FF00", pady=20)
         titol.pack()
-
-        subtitol = Label(guia, text=f"Mode Lliure:\n{Descripcions[1]}\n\nMode Carrera:\n{Descripcions[2]}\n\nMode Supervivència:\n{Descripcions[3]}", font=("Helvetica", 12), 
-                      bg="#1a1a1a", fg="white", pady=10)
-        subtitol.pack()
-
+        if num_jugadors == 1:
+            subtitol = Label(guia, text=f"Mode Lliure:\n{Descripcions[1]}\n\nMode Carrera:\n{Descripcions[2]}\n\nMode Supervivència:\n{Descripcions[3]}", font=("Helvetica", 12), 
+                        bg="#1a1a1a", fg="white", pady=10)
+            subtitol.pack()
+        else:
+            subtitol = Label(guia, text=f"Mode Lliure:\n{Descripcions[1]}\n\nMode Carrera:\n{Descripcions[2]}\n", font=("Helvetica", 12), 
+                        bg="#1a1a1a", fg="white", pady=10)
+            subtitol.pack()
         peu_de_pagina_guia = Label(guia, text="Tanqui la finestra per tornar a la selecció de mode", font=("Arial", 10), 
                    bg="#1a1a1a", fg="gray", pady=20)
         peu_de_pagina_guia.pack(side=BOTTOM)
@@ -89,7 +92,7 @@ def destrueixFinestra(mode_escollit, menu, mode):
         guia.mainloop()
 
 
-def crearMenu():
+def crearMenu(num_jugadors):
     menu = Tk()
     menu.title("Selecció de Mode")
     menu.geometry("400x400")
@@ -115,17 +118,17 @@ def crearMenu():
                   width=25, height=2, bd=0, cursor="hand2",
                   command=lambda: destrueixFinestra(2, menu, mode))
     btn2.pack(pady=5)
-
-    btn3 = Button(menu, text=Noms_modes[3], font=("Helvetica", 14),
-                  bg="#333333", fg="white", activebackground="red",
-                  width=25, height=2, bd=0, cursor="hand2",
-                  command=lambda: destrueixFinestra(3, menu, mode))
-    btn3.pack(pady=5)
+    if num_jugadors == 1:
+        btn3 = Button(menu, text=Noms_modes[3], font=("Helvetica", 14),
+                    bg="#333333", fg="white", activebackground="red",
+                    width=25, height=2, bd=0, cursor="hand2",
+                    command=lambda: destrueixFinestra(3, menu, mode))
+        btn3.pack(pady=5)
 
     btn4 = Button(menu, text=Noms_modes[4], font=("Helvetica", 11),
                   bg="#333333", fg="white", activebackground="red",
                   width=20, height=2, bd=0, cursor="hand2",
-                  command=lambda: destrueixFinestra(4, menu, mode))
+                  command=lambda: destrueixFinestra(4, menu, mode,num_jugadors))
     btn4.pack(pady=5)
 
     peu_de_pagina = Label(menu, text="Pulsa per seleccionar", font=("Arial", 8), 
